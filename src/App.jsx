a3c5c23d463d3011c8720b090e0c08d2b4cf54ac
Lucas Arend Maciel <lucas.m.arend@gmail.com>
2025-08-27 16:16:41 -0300

    Correção de conflitos com merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import DefaultLayout from './layouts/default-layout';
-import Dashboard from './pages/dashboard';
 import Login from "./components/Login";
-import Dashboard2 from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
 import Menu from "./Menu";
+import { AuthProvider } from "./context/AuthContext";
 import NotFound from "./components/NotFound";
 import Transacao from "./components/Transaction/Transaction";
 import Register from "./components/Register"
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 
-export default function App() {
+
+
+function App() {
   return (
     <AuthProvider>
       <Router>
         <Menu />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login />} /> 
           <Route path="/register" element={<Register />} />
-
-
-          <Route
-            path="/dashboard"
-            element={
-              <DefaultLayout>
-                <Dashboard />
-              </DefaultLayout>
-            }
-          />
-          <Route
-            path="/transacao"
-            element={
-              <DefaultLayout>
-                <Transacao />
-              </DefaultLayout>
-            }
-          />
-
+          <Route path="/transacao" element={<Transacao />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
+    </Router>
     </AuthProvider>
   );
 }
+
+export default App;
